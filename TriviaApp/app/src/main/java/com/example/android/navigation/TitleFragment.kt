73e9fb8f62416f5498337c4d61133d4ee50ac5d6
Taufan Erfiyanto <taufan_erfiyanto@linkaja.id>
2022-04-01 10:51:56 +0700

    Add TriviaApp Project

---
+++ TriviaApp/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,55 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TitleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TitleFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_title, container, false)
+
+
+        setHasOptionsMenu(true)
+
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        )
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+}
