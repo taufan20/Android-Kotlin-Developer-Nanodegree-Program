7706582f2fc9af778b9cc2fb880c54185ab475dc
taufan <taufan_erfiyanto@linkaja.id>
2022-09-07 21:48:15 +0700

    Add MotionLayout & Styles

--- Political Preparedness/app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeBindingAdapters.kt
+++ Political Preparedness/app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeBindingAdapters.kt
@@ -5,12 +5,23 @@ import android.widget.ImageView
 import android.widget.Spinner
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.android.politicalpreparedness.R
+
 
 @BindingAdapter("profileImage")
 fun fetchImage(view: ImageView, src: String?) {
     src?.let {
         val uri = src.toUri().buildUpon().scheme("https").build()
         //TODO: Add Glide call to load image and circle crop - user ic_profile as a placeholder and for errors.
+
+        Glide
+            .with(view.context)
+            .load(uri)
+            .apply(RequestOptions.circleCropTransform())
+            .placeholder(R.drawable.ic_profile)
+            .into(view)
     }
 }
 
